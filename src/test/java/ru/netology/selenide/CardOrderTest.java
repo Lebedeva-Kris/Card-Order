566a1a1114b1bdcccbebd98760551db17186f281
Kristina <kris2015.lebedeva@yandex.ru>
2020-08-08 02:29:48 +0300

    invalid tests added

--- src/test/java/ru/netology/selenide/CardOrderTest.java
+++ src/test/java/ru/netology/selenide/CardOrderTest.java
@@ -23,6 +23,43 @@ public class CardOrderTest {
         form.$("[data-test-id='phone'] input").setValue("+79999999999");
         form.$("[data-test-id='agreement']").click();
         form.$("[type='button']").click();
+
         $("[data-test-id='order-success']").shouldHave(text("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldEnterInvalidName() {
+        open("http://localhost:9999");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name'] input").setValue("Kris777");
+        form.$("[data-test-id='phone'] input").setValue("+79999999999");
+        form.$("[data-test-id='agreement']").click();
+        form.$("[type='button']").click();
+
+        $("[data-test-id='name'].input_invalid");
+    }
+
+    @Test
+    void shouldEnterInvalidPhoneNumber() {
+        open("http://localhost:9999");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name'] input").setValue("Кристина");
+        form.$("[data-test-id='phone'] input").setValue("123456789");
+        form.$("[data-test-id='agreement']").click();
+        form.$("[type='button']").click();
+
+        $("[data-test-id='phone'].input_invalid");
+    }
+
+    @Test
+    void shouldUnMarkedCheckbox() {
+        open("http://localhost:9999");
+        SelenideElement form = $("#root");
+        form.$("[data-test-id='name'] input").setValue("Кристина");
+        form.$("[data-test-id='phone'] input").setValue("+79999999999");
+        form.$("[type='button']").click();
+
+        $("[data-test-id=agreement].input_invalid");
+    }
+
 }
